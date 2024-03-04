7a3d644c17059212d2c0f8bb23d73e8d3a31f3e1
Chafiiiiii <kevin.casagualpa@epn.edu.ec>
2024-03-04 04:43:54 -0500

    Netflix R.View

---
+++ 08-Netflix/03-Netflix/app/src/main/java/com/fraporitmostech/kotflix/Endpoints.kt
@@ -0,0 +1,9 @@
+package com.fraporitmostech.kotflix
+
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface Endpoints {
+    @GET("/FranEspino/demo/db")
+    suspend  fun getDataMovies(): Response<MovieResponse>
+}
---
+++ 08-Netflix/03-Netflix/app/src/main/java/com/fraporitmostech/kotflix/OnClickListener.kt
@@ -0,0 +1,7 @@
+package com.fraporitmostech.kotflix
+
+interface OnClickListener {
+    fun OnClick(movie: Movie){
+
+    }
+}
